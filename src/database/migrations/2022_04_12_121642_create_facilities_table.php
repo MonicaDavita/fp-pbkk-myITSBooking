695be97cde9de64010c48ebdb29f7986a659f62c
christhopermarcelino <christhopermarcelino@gmail.com>
2022-04-12 19:25:42 +0700

    feat: add table migrations

---
+++ src/database/migrations/2022_04_12_121642_create_facilities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->enum('category', ['general, sport, department']);
+            $table->time('open_time');
+            $table->time('close_time');
+            $table->integer('quota');
+            $table->timestamps();
+
+            $table->foreignId('department_id')->constrained('departments')->nullable();
+            $table->foreignId('admin_id')->constrained('admins');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
---
+++ src/database/migrations/2022_04_12_121856_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('book_date');
+            $table->float('price', 10, 2);
+            $table->integer('duration');
+            $table->integer('participants');
+            $table->binary('document');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreignId('admin_id')->constrained('admins');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('facility_id')->constrained('facilities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
