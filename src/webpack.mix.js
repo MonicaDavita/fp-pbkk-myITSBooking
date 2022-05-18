c17219501f154f38cea2a1eefad490071cdae1a6
christhopermarcelino <christhopermarcelino@gmail.com>
2022-05-18 20:14:17 +0700

    feat: migrate styling to bootstrap 5

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,5 +1,4 @@
-const mix = require("laravel-mix");
-const tailwindcss = require("tailwindcss");
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,14 +6,11 @@ const tailwindcss = require("tailwindcss");
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.css", "public/css", tailwindcss)
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss("./tailwind.config.js")],
-    });
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
