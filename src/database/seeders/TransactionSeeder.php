19d0abaa56bb752a16db1969ef5d80b9da1d2c6d
christhopermarcelino <christhopermarcelino@gmail.com>
2022-06-01 18:50:16 +0700

    refactor: re-adjust facilities seeder and navbar rendering

--- src/database/seeders/TransactionSeeder.php
+++ src/database/seeders/TransactionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Transaction;
 
 class TransactionSeeder extends Seeder
 {
@@ -14,6 +15,59 @@ class TransactionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Transaction::create([
+            'book_date' => now(),
+            'duration' => 3,
+            'participants' => 50,
+            'document' => 10101,
+            'description' => 'Halal Bihalal',
+            'admin_id' => 1,
+            'user_id' => 1,
+            'facility_id' => 1,
+        ]);
+
+        Transaction::create([
+            'book_date' => now(),
+            'duration' => 2,
+            'participants' => 50,
+            'document' => 10101,
+            'description' => 'Halal Bihalal',
+            'admin_id' => 2,
+            'user_id' => 1,
+            'facility_id' => 2,
+        ]);
+
+        Transaction::create([
+            'book_date' => now(),
+            'duration' => 4,
+            'participants' => 50,
+            'document' => 10101,
+            'description' => 'Halal Bihalal',
+            'admin_id' => 1,
+            'user_id' => 2,
+            'facility_id' => 3,
+        ]);
+
+        Transaction::create([
+            'book_date' => now(),
+            'duration' => 2,
+            'participants' => 50,
+            'document' => 10101,
+            'description' => 'Halal Bihalal',
+            'admin_id' => 2,
+            'user_id' => 2,
+            'facility_id' => 4,
+        ]);
+
+        Transaction::create([
+            'book_date' => now(),
+            'duration' => 1,
+            'participants' => 50,
+            'document' => 10101,
+            'description' => 'Halal Bihalal',
+            'admin_id' => 1,
+            'user_id' => 3,
+            'facility_id' => 5,
+        ]);
     }
 }
